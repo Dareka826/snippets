d1d7268e4bac326fe191cb858016b597251d93e9
Rin Brk <minecraftredstonemaniak@gmail.com>
2022-11-23 18:28:27 +0100

    Add an implementation for an arena allocator

---
+++ arena.c
@@ -0,0 +1,125 @@
+#include "arena.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef ARENA_MEM_DBG
+#define C_RESET  "\033[0m"
+#define C_GREEN  "\033[32m"
+#define C_YELLOW "\033[33m"
+#define C_BLUE   "\033[34m"
+#endif
+
+// Init / Deinit {{{
+Arena* create_arena() { /*{{{*/
+    Arena *ap = malloc(sizeof(Arena));
+    memset(ap, 0, sizeof(Arena));
+
+#ifdef ARENA_MEM_DBG
+    fprintf(stderr, C_GREEN"[MEM] "C_YELLOW"malloc: "C_RESET" %p\n", ap);
+#endif
+
+    return ap;
+} /*}}}*/
+
+void free_arena(Arena* ap) { /*{{{*/
+    struct ArenaBuf
+        *head = ap->head,
+        *next = NULL;
+
+    while (head != NULL) {
+        next = head->next;
+
+#ifdef ARENA_MEM_DBG
+        fprintf(stderr, C_GREEN"[MEM] "C_BLUE"free: "C_RESET" %p\n", head->ptr);
+#endif
+        free(head->ptr);
+
+#ifdef ARENA_MEM_DBG
+        fprintf(stderr, C_GREEN"[MEM] "C_BLUE"free: "C_RESET" %p\n", head);
+#endif
+        free(head);
+
+        head = next;
+    }
+
+#ifdef ARENA_MEM_DBG
+    fprintf(stderr, C_GREEN"[MEM] "C_BLUE"free: "C_RESET" %p\n", ap);
+#endif
+    free(ap);
+} /*}}}*/
+// }}}
+
+// Allocate {{{
+void* arena_alloc(Arena* ap, size_t bytes) { /*{{{*/
+    struct ArenaBuf *abp = malloc(sizeof(struct ArenaBuf));
+
+#ifdef ARENA_MEM_DBG
+    fprintf(stderr, C_GREEN"[MEM] "C_YELLOW"malloc: "C_RESET" %p\n", abp);
+#endif
+
+    abp->ptr = malloc(bytes);
+
+#ifdef ARENA_MEM_DBG
+    fprintf(stderr, C_GREEN"[MEM] "C_YELLOW"malloc: "C_RESET" %p\n", abp->ptr);
+#endif
+
+    abp->next = ap->head;
+    ap->head = abp;
+
+    return abp->ptr;
+} /*}}}*/
+
+void* arena_alloc0(Arena* ap, size_t bytes) { /*{{{*/
+    struct ArenaBuf *abp = malloc(sizeof(struct ArenaBuf));
+
+#ifdef ARENA_MEM_DBG
+    fprintf(stderr, C_GREEN"[MEM] "C_YELLOW"malloc: "C_RESET" %p\n", abp);
+#endif
+
+    abp->ptr = calloc(1, bytes);
+
+#ifdef ARENA_MEM_DBG
+    fprintf(stderr, C_GREEN"[MEM] "C_YELLOW"calloc: "C_RESET" %p\n", abp->ptr);
+#endif
+
+    abp->next = ap->head;
+    ap->head = abp;
+
+    return abp->ptr;
+} /*}}}*/
+// }}}
+
+#ifdef ARENA_EXTRA
+// Extra functions {{{
+void arena_mid_free(Arena *ap, void *ptr) {
+    struct ArenaBuf
+        *head = ap->head,
+        *prev = NULL;
+
+    while (head != NULL) {
+        if (head->ptr == ptr) {
+            // Remove from linked list
+            if (prev != NULL)
+                 prev->next = head->next;
+            else ap->head   = head->next;
+
+            // Free
+#ifdef ARENA_MEM_DBG
+        fprintf(stderr, C_GREEN"[MEM] "C_BLUE"free: "C_RESET" %p\n", head->ptr);
+#endif
+        free(head->ptr);
+
+#ifdef ARENA_MEM_DBG
+        fprintf(stderr, C_GREEN"[MEM] "C_BLUE"free: "C_RESET" %p\n", head);
+#endif
+        free(head);
+
+            break;
+        }
+
+        prev = head;
+        head = head->next;
+    }
+}
+// }}}
+#endif
---
+++ arena.h
@@ -0,0 +1,34 @@
+#ifndef __ARENA_ALLOC_H_mv4ndFREGmFcCdiL
+#define __ARENA_ALLOC_H_mv4ndFREGmFcCdiL
+
+#include <stdlib.h>
+
+typedef  struct Arena  Arena;
+
+// Singly-linked list of memory buffers
+struct ArenaBuf {
+    struct ArenaBuf *next;
+    void *ptr;
+};
+
+// Arena allocator
+struct Arena {
+    struct ArenaBuf *head;
+};
+
+// Init / Destroy
+Arena* create_arena();
+
+void free_arena(Arena*);
+#define nfree_arena(ptr) do { free_arena(ptr); ptr = NULL; } while(0);
+
+// Allocate
+void* arena_alloc(Arena*, size_t);
+void* arena_alloc0(Arena*, size_t);
+
+#ifdef ARENA_EXTRA
+// Frees a single buffer from the list (for rare cases)
+void arena_mid_free(Arena*, void*);
+#endif
+
+#endif
