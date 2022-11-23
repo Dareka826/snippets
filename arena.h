3f2609da34c061d661d7144655c9657227bd5364
Rin Brk <minecraftredstonemaniak@gmail.com>
2022-11-23 18:51:39 +0100

    Arena: Add parameter names to function definitions

--- arena.h
+++ arena.h
@@ -20,22 +20,22 @@ struct Arena {
 // Init / Destroy
 Arena* create_arena();
 
-void free_arena(Arena*);
+void free_arena(Arena *ap);
 #define nfree_arena(ptr) do { free_arena(ptr); ptr = NULL; } while(0);
 
 
 // Allocate
-void* arena_alloc(Arena*, size_t);
-void* arena_alloc0(Arena*, size_t);
+void* arena_alloc(Arena *ap, size_t size);
+void* arena_alloc0(Arena *ap, size_t size);
 
-void* arena_xalloc(Arena*, size_t);
-void* arena_xalloc0(Arena*, size_t);
+void* arena_xalloc(Arena *ap, size_t size);
+void* arena_xalloc0(Arena *ap, size_t size);
 
 
 #ifdef ARENA_EXTRA
 // Frees a single buffer from the list (for rare cases)
-void arena_mid_free(Arena*, void*);
-#define arena_mid_nfree(ap,ptr) do { arena_mid_free(ap, ptr); ptr = NULL; } while(0);
+void arena_mid_free(Arena *ap, void *ptr);
+#define arena_mid_nfree(ap, ptr) do { arena_mid_free(ap, ptr); ptr = NULL; } while(0);
 #endif
 
 #endif
