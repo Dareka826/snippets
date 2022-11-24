a7cd9df249c2bc04e63bdf4574359744e5799076
Rin Brk <dareka826@gmail.com>
2022-11-24 17:32:49 +0100

    Arena: Make it explicit that mid-free is not recommended

--- arena/arena.h
+++ arena/arena.h
@@ -42,12 +42,14 @@ void* arena_xalloc0(Arena * const ap, size_t size);
 
 
 #ifdef ARENA_EXTRA
+size_t arena_get_size(Arena const * const ap);
+size_t arena_get_buffer_size(Arena const * const ap, void const * const ptr);
+
+#ifdef ARENA_UNSAFE
 // Frees a single buffer from the list (for rare cases)
 void arena_mid_free(Arena * const ap, void * const ptr);
 #define arena_mid_nfree(ap, ptr) do { arena_mid_free(ap, ptr); ptr = NULL; } while (0)
-
-size_t arena_get_size(Arena const * const ap);
-size_t arena_get_buffer_size(Arena const * const ap, void const * const ptr);
+#endif
 #endif
 
 #endif
